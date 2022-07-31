e381006d2eb2392e4a886847a19a04d1d184f247
MakeithappenMike <omichaelaaron@gmail.com>
2022-07-31 15:03:09 -0400

    swapped current day to jquery and building out rows

--- Develop/script.js
+++ Develop/script.js
@@ -13,6 +13,22 @@
 // THEN the saved events persist
 
 // Get the current day and show it at the top of the calendar
-var currentDayContainer = document.getElementById("currentDay");
+var currentDayContainer = $("#currentDay");
 var currentDay = moment();
-currentDayContainer.innerHTML = currentDay.format("dddd, MMMM Do YYYY");
+currentDayContainer.html(currentDay.format("dddd, MMMM Do YYYY"));
+// currentDayContainer.innerHTML = currentDay.format("dddd, MMMM Do YYYY");
+
+// Build rows
+var row = "";
+for (i = 9; i <= 18; i++) {
+    console.log(i);
+    var row = $('#row').append(`<section class="row">${i}</section>`);
+    col1 = $('#time').append(`<section class="col">${i}</section>`);
+    col2 = $('#info').append(`<section class="col">${i}</section>`);
+    col3 = $('#save').append(`<section class="col">${i}</section>`);
+    row.append(col1);
+    row.append(col2);
+    row.append(col3);
+    // ${"#container"}.append(row);
+};
+
