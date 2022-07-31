11d9432c252925b41875e04fd148b57b802872e5
MakeithappenMike <omichaelaaron@gmail.com>
2022-07-31 16:06:19 -0400

    numbers showing as time

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
+// THEN the current day is displayed at the top of the calendar - done
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
 // WHEN I view the time blocks for that day
@@ -21,16 +21,30 @@ currentDayContainer.html(currentDay.format("dddd, MMMM Do YYYY"));
 
 // Build rows
 var row = "";
+
+// Set's hours to 9am - 6pm
 for (i = 9; i <= 18; i++) {
-    // console.log(i);
     row = $(`<section class="row time-block">`);
-    // console.log(row);
-    col1 = $(`<section class="col hour">${i}</section>`);
+    col1 = $(`<section class="col hour">${convertAmPm(i)}</section>`);
     col2 = $(`<textarea class="col description">DESCRIPTION</textarea>`);
-    col3 = $(`<section class="col saveBtn">SAVE</section></section>`);
+    col3 = $(`<section class="col saveBtn"><i class="fas fa-save"></i></section></section>`);
     row.append(col1);
     row.append(col2);
     row.append(col3);
     calendarContainer.append(row);
+};
+
+// Convert time to AM or PM
+function convertAmPm(hour) {
+    var amOrPm = "";
+    if (hour <= 11){
+        amOrPm = "AM";
+      } else{
+        amOrPm = "PM";
+      }
+      // Get 13 to show as 1
+      hour = hour % 12;
+      hour = hour ? hour : 12;
+      return hour + " " + amOrPm;
 };
 
