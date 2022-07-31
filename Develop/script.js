96245033863a4ede92e16aa3cffd45e435a4f43f
MakeithappenMike <omichaelaaron@gmail.com>
2022-07-31 16:15:51 -0400

    working on color coding hours

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar - done
 // WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
+// THEN I am presented with time blocks for standard business hours - done
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
 // WHEN I click into a time block
@@ -16,6 +16,8 @@
 var currentDayContainer = $("#currentDay");
 var calendarContainer = $(".container");
 var currentDay = moment();
+var currentHour = moment().format("h");
+console.log(currentHour);
 currentDayContainer.html(currentDay.format("dddd, MMMM Do YYYY"));
 // currentDayContainer.innerHTML = currentDay.format("dddd, MMMM Do YYYY");
 
@@ -26,12 +28,17 @@ var row = "";
 for (i = 9; i <= 18; i++) {
     row = $(`<section class="row time-block">`);
     col1 = $(`<section class="col hour">${convertAmPm(i)}</section>`);
+    col1.attr("background-color", "black");
     col2 = $(`<textarea class="col description">DESCRIPTION</textarea>`);
     col3 = $(`<section class="col saveBtn"><i class="fas fa-save"></i></section></section>`);
     row.append(col1);
     row.append(col2);
     row.append(col3);
     calendarContainer.append(row);
+
+    if (convertAmPm(i) > currentHour) {
+        col1.attr("style", "background-color: red");
+    };
 };
 
 // Convert time to AM or PM
