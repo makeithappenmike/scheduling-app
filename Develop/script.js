465437b0a89b098a4a2e1d3fc4a4d852424b631a
MakeithappenMike <omichaelaaron@gmail.com>
2022-08-04 17:42:09 -0400

    working on getting localStorage via jquery forEach

--- Develop/script.js
+++ Develop/script.js
@@ -9,21 +9,39 @@ var description;
 var targetId;
 var targetDescription;
 // var calendarTimeBlocks = [];
-console.log("current hour:", currentHour);
-console.log("test", moment().set('hour', 9).format("ha"));
+// console.log("current hour:", currentHour);
+// console.log("test", moment().set('hour', 9).format("ha"));
 currentDayContainer.html(currentDay.format("[Today is] dddd, MMMM Do YYYY"));
 // currentDayContainer.innerHTML = currentDay.format("dddd, MMMM Do YYYY");
 
 // Build rows
 var row = "";
 
+// Check for localStorage
+$(".hour").each(function () {
+    var existingTime = $(this).val();
+    var existingDescription = localStorage.getItem(existingTime);
+    console.log(existingDescription);
+});
+// var nineAm = localStorage.getItem("#description9");
+// var testAm = localStorage.getItem("#description10");
+// var elevenAm = localStorage.getItem("#description11");
+// var twelvePm = localStorage.getItem("#description12");
+// var onePm = localStorage.getItem("#description13");
+// var twoPm = localStorage.getItem("#description14");
+// var threePm = localStorage.getItem("#description15");
+// var fourPm = localStorage.getItem("#description16");
+// var fivePm = localStorage.getItem("#description17");
+// var sixPm = localStorage.getItem("#description18");
+
+if (nineAm != null) {
+    console.log("local:", nineAm);
+} else if (testAm != null) {
+    console.log("local:", testAm);
+};;
+
 // Sets hours to 9am - 6pm
 for (i = 9; i <= 18; i++) {
-
-    // Check for localStorage
-    if (localStorage.getItem(`description${i}` != null)) {
-        
-    };
 
     // Create objects for time blocks
     var timeBlock = {"id": i, "time": moment().set('hour', i).format("ha"), "description": "desc"};
@@ -34,7 +52,7 @@ for (i = 9; i <= 18; i++) {
     col1 = $(`<section class="col hour">${moment().set('hour', i).format("ha")}</section>`);
     col1.attr("background-color", "black");
     col2 = $(`<textarea id="description${i}" class="col description"></textarea>`);
-    col3 = $(`<section class="col saveBtn"><i id="description${i}saveBtn" class="fas fa-save saveIcon"></i></section></section>`);
+    col3 = $(`<section class="col saveBtn"><i id="saveBtn${i}" class="fas fa-save saveIcon"></i></section></section>`);
     row.append(col1);
     row.append(col2);
     row.append(col3);
@@ -67,9 +85,9 @@ saveIcon.click(function(event){
     targetId = event.target.id.match(/\d+/).toString();
     targetDescription = "#description" + targetId;
     localStorage.setItem(targetDescription, $(targetDescription).val());
-    console.log("target ID:", targetId);
-    console.log("target Desc:", targetDescription);
-    console.log("prev", $(targetDescription).val());
+    // console.log("target ID:", targetId);
+    // console.log("target Desc:", targetDescription);
+    // console.log("prev", $(targetDescription).val());
   });
 
 
